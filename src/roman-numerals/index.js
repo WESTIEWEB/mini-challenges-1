975f76a4cff35f4601b380de884d725e9c8cd7e3
Chibuike-ilo <chibuike.ilo@decagon.dev>
2022-09-13 07:10:05 +0100

    my_first_commit

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,42 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    // we first to for have to create an object of key values pairs for all primary conversion parameters
+
+    const conversion = {
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000
+    };
+
+    let converted= []; //this is a container for our conversion
+    
+    // we have to iterate through each integer in roman
+    for (let i=0; i < roman.length; i++) {
+
+        // here we check if the first digit of roman is greater or less than the subsequent one
+        if (conversion[roman[i]] < conversion[roman[i+1]] ) {
+
+            // if lesser, we will just push the result when the second int is substracted from the first
+            converted.push(conversion[roman[i+1]] - conversion[roman[i]]);
+            i++
+
+            // continue;
+
+        }
+
+        else {
+            //if greater
+            converted.push(conversion[roman[i]]);
+        }
+    }
+
+    return(converted.reduce((total, current) => total + current));
+}
 
 module.exports = romanToDecimal;
