ee39dd1d20b78d91217272de18482bf5baa2ba59
Chibuike-ilo <chibuike.ilo@decagon.dev>
2022-09-13 07:45:02 +0100

    edited_version of bracket

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -1,9 +1,83 @@
+const { validateLocaleAndSetLanguage } = require("typescript");
+
 /**
  * Implement the solution to brackets
  *
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+ function isValid (str) {
+    
+    let stack = [];
+    
+    // since balance bracket means for every opening brace, there must be a closing brace
+    if (str.length % 2 !==0) {
+        return "invalid";
+    }
+    
+    //iterating through every item in str
+    for (let c of str) {
+        
+        // this line of code creates an array of only opening braces
+        if ('({['.includes(c)) {
+            stack.push(c);
+        
+        
+            // this line of code check if their is an equivalent closing brace for every opneing brace in the stack array.
+            if (']})'.includes(c)) {
+            
+                let check = stack.pop(); //  that is to say for every closing tag ')' , pop out the equivalent opening tag '('.
+        
+                if (!(c === ')' && check === '(' || c === '}' && check === '{' || c === ']' && check === '[')) {
+                     // let check = stack.pop();
+                    stack.push(c);
+
+                     // return "invalid";
+                
+            
+             
+                    if (stack[stack.length-1] === '') {
+                        return "valid";
+                     }
+                     if (stack.length !==0) {
+                        return "invalid";
+                    }   
+                if( c === '{' && stack.pop() == '{') {
+                    return 'invalid';
+                }
+                if( c === '[' && stack.pop() == '[') {
+                    return 'invalid';
+                }
+                if( c === '(' && stack.pop() == '(') {
+                    return 'invalid';
+                }
+                }
+                
+                
+                else {
+                    return "valid";
+                }
+            }
+            
+            
+                
+            
+            
+        }  
+
+        if (!('[({'.includes(c))){
+            return "valid";
+        }  
+        
+    }
+
+       
+      
+
+   if(stack.length == 0) {
+    return "valid";
+   }
+    
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,28 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+    let result = []; //since we are return an array of our transposed items, we need to declare a container for them
+
+    let array_len = array.length; // to avoid continous repeatation of array.length method
+    let content_len = array[0].length; //this line of code helps us to store the length of each items in our 2D array 
+
+    for (let m =0; m<content_len; m++)  {
+        result.push([]); // this is to say create append an empty list content_len times
+    }
+
+    for ( let i=0; i<array_len; i++){ // we iterate through the contents of our array array_len times
+
+        for (let j=0; j<content_len; j++) { 
+            
+            result[j].push(array[i][j])
+        }
+        
+    }
+
+    return result;
+
+}
 
 module.exports = transpose;
